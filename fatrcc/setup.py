0dd8dbb2ce06d5cf9772b06c37d0d76eeb97f784
RCCEnjoyer <167428007+RCCEnjoyer@users.noreply.github.com>
2024-08-29 11:18:20 +0200

    Update setup.py

--- fatrcc/setup.py
+++ fatrcc/setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION = ''
 setup(
        # the name must match the folder name 'verysimplemodule'
         name="fatrcc",
-        version="0.0.1",
+        version="0.0.3",
         author="Luis Angel Fernandez",
         author_email="<lafernandez@essbilbao.org>",
         description=DESCRIPTION,
